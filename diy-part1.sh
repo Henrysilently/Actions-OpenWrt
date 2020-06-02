54e5caa3535d3c67119420c987190ffe6b51b75b
Henrysilently <xu_wenhan@live.cn>
2020-06-02 18:05:14 +0800

    del adguardhome link

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,4 @@
 # Add a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
+#sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
